74662a4c2591c548dce8537df350a2da278919d0
ViktorOlsson <vicke.olsson81@gmail.com>
2019-09-25 22:12:20 +0200

    Added function to get all unicorns

--- index.php
+++ index.php
@@ -1,5 +1,36 @@
 <?php
+  require 'vendor/autoload.php';
 
+  function run(){
+    if(isset($_GET['unicorn'])){
+      echo "To be done";
+    }
+    else if(isset($_GET['subject'])){
+      echo "To be done";
+    }
+    else{
+      allUnicorns();
+    }
+  }
+
+  function allUnicorns(){
+    $headers = array('Accept' => 'application/json');
+    $response = Unirest\Request::get("http://unicorns.idioti.se", $headers);
+    $response->body;        
+    $response->raw_body;
+    $result = json_decode($response->raw_body, true);
+    foreach($result as $item){
+      $id = $item['id'];
+      $name = $item['name'];
+      if(!empty($name)){
+        echo "<p>$id $name</p>  
+        <form action='index.php' method='GET'>
+        <button name='subject' type='submit' value='$id'>Visa mer</button>
+        </form>";
+        
+      }  
+    } 
+    }
 ?>
 
 
@@ -20,5 +51,9 @@
   <form action="index.php" method="GET">
     <input type="submit" name="submit" value="Visa alla">
   </form>
+  <div id="container">
+  <?php
+    run();
+  ?>
   </body>
 </html>
