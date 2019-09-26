fdb282a6cf9f6869291ce7d8f334c4c115be6502
ViktorOlsson <vicke.olsson81@gmail.com>
2019-09-26 12:09:09 +0200

    Fixed picture size and added updated form

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
       $id = $item['id'];
       $name = $item['name'];
       if(!empty($name)){
-        echo "<p>$id $name</p>  
+        echo "<p>$id. $name</p>  
         <form action='index.php' method='GET'>
         <button class='btn btn-success' name='subject' type='submit' value='$id'>Visa mer</button>
         </form>";
@@ -117,15 +117,15 @@
   <body>
   <div class="container">
     <div class="col-xs-12 text-center mt-5">
-      <div class="form-group">
       <form action="index.php" method="GET">
-        <label for="unicorn">Id på enhörning</label>
+        <div class="form-group">
+        <h4>Id på enhörning</h4>
         <div class="btn-group">
           <input class="form-control" type="text" name="unicorn" id="unicorn">
           <input class="btn btn-primary" type="submit" name="submit">
         </div>
+        </div>
       </form>
-      </div>
     </div>
 
     <div class="col-xs-12 text-center">
@@ -137,9 +137,10 @@
   </div>
   
   <div id="content-div" class="container text-center">
-  <?php
-    run();
-  ?>
+    <?php
+      run();
+    ?>
+  </div>
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
