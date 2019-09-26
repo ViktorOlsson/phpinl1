b0347016a17123adbdbcd5e021fcf4a7438abbb1
ViktorOlsson <vicke.olsson81@gmail.com>
2019-09-26 11:48:16 +0200

    Added a css file and bootstrap code for styling

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
       if(!empty($name)){
         echo "<p>$id $name</p>  
         <form action='index.php' method='GET'>
-        <button name='subject' type='submit' value='$id'>Visa mer</button>
+        <button class='btn btn-success' name='subject' type='submit' value='$id'>Visa mer</button>
         </form>";
         
       }  
@@ -112,20 +112,36 @@
     <meta charset="utf-8">
     <title>Enhörningar</title>
     <link rel="stylesheet" type="text/css" href="./style.css"/>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
   <body>
-  <form action="index.php" method="GET">
-    <label for="unicorn">Id på enhörning</label>
-    <input type="text" name="unicorn" id="unicorn">
-    <input type="submit" name="submit">
-  </form>
+  <div class="container">
+    <div class="col-xs-12 text-center mt-5">
+      <div class="form-group">
+      <form action="index.php" method="GET">
+        <label for="unicorn">Id på enhörning</label>
+        <div class="btn-group">
+          <input class="form-control" type="text" name="unicorn" id="unicorn">
+          <input class="btn btn-primary" type="submit" name="submit">
+        </div>
+      </form>
+      </div>
+    </div>
 
-  <form action="index.php" method="GET">
-    <input type="submit" name="submit" value="Visa alla">
-  </form>
-  <div id="container">
+    <div class="col-xs-12 text-center">
+      <form action="index.php" method="GET">
+        <input class="btn btn-primary" type="submit" name="submit" value="Visa alla">
+      </form>
+    </div>
+    <hr>
+  </div>
+  
+  <div id="content-div" class="container text-center">
   <?php
     run();
   ?>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   </body>
 </html>
